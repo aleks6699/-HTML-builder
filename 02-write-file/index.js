7001e954f0e8ca2818858dfcedac2f0a7172c0e0
Oleksandr <aleks6699@gmail.com>
2024-01-20 14:15:04 +0200

    feat: add 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+let writeStream = fs.createWriteStream(filePath);
+const { stdin, stdout } = process;
+const exitNotification = () => {
+  stdout.write('До свидания!');
+  process.exit();
+};
+
+stdout.write('Пожалуйста, введите текст\n');
+
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    exitNotification();
+  }
+  writeStream.write(data);
+});
+
+process.on('SIGINT', exitNotification);
